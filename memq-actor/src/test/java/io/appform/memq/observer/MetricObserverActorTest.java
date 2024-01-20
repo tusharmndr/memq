915fdf729295ef14ff0ccf31bf4abe6a31a52218
tushar <tushar@phonepe.com>
2024-01-20 11:00:02 +0530

    closing threadpool executor in test

--- memq-actor/src/test/java/io/appform/memq/observer/MetricObserverActorTest.java
+++ memq-actor/src/test/java/io/appform/memq/observer/MetricObserverActorTest.java
@@ -20,40 +20,48 @@ class MetricObserverActorTest {
 
     @Test
     void testMetrics() {
-        val metricPrefix =  "actor." + TestUtil.HighLevelActorType.EXCEPTION_ACTOR.name() + ".";
-        val counter = new AtomicInteger();
-        val sideline = new AtomicBoolean();
         val tc = Executors.newFixedThreadPool(TestUtil.DEFAULT_THREADPOOL_SIZE);
-        val actorConfig = TestUtil.noRetryActorConfig(Constants.SINGLE_PARTITION);
-        val actorSystem = TestUtil.actorSystem(tc);
-        val actor = TestUtil.allExceptionActor(counter, sideline, actorConfig, actorSystem);
-        actor.publish(new TestIntMessage(1));
-        Awaitility.await()
-                .timeout(Duration.ofMinutes(1))
-                .catchUncaughtExceptions()
-                .until(() -> counter.get() == 1);
-        val metrics = actorSystem.metricRegistry().getMetrics();
-        assertEquals(13, metrics.size());
-        assertEquals(1, ((Meter) metrics.get(metricPrefix + ActorOperation.PUBLISH.name() + ".total")).getCount());
-        assertEquals(1, ((Meter) metrics.get(metricPrefix + ActorOperation.HANDLE_EXCEPTION.name() + ".total")).getCount());
-        assertEquals(1, ((Meter) metrics.get(metricPrefix + ActorOperation.CONSUME.name() + ".total")).getCount());
+        try {
+            val metricPrefix = "actor." + TestUtil.HighLevelActorType.EXCEPTION_ACTOR.name() + ".";
+            val counter = new AtomicInteger();
+            val sideline = new AtomicBoolean();
+            val actorConfig = TestUtil.noRetryActorConfig(Constants.SINGLE_PARTITION);
+            val actorSystem = TestUtil.actorSystem(tc);
+            val actor = TestUtil.allExceptionActor(counter, sideline, actorConfig, actorSystem);
+            actor.publish(new TestIntMessage(1));
+            Awaitility.await()
+                    .timeout(Duration.ofMinutes(1))
+                    .catchUncaughtExceptions()
+                    .until(() -> counter.get() == 1);
+            val metrics = actorSystem.metricRegistry().getMetrics();
+            assertEquals(13, metrics.size());
+            assertEquals(1, ((Meter) metrics.get(metricPrefix + ActorOperation.PUBLISH.name() + ".total")).getCount());
+            assertEquals(1, ((Meter) metrics.get(metricPrefix + ActorOperation.HANDLE_EXCEPTION.name() + ".total")).getCount());
+            assertEquals(1, ((Meter) metrics.get(metricPrefix + ActorOperation.CONSUME.name() + ".total")).getCount());
+        } finally {
+            tc.shutdownNow();
+        }
     }
 
     @Test
     void testNoMetrics() {
-        val counter = new AtomicInteger();
-        val sideline = new AtomicBoolean();
         val tc = Executors.newFixedThreadPool(TestUtil.DEFAULT_THREADPOOL_SIZE);
-        val actorConfig = TestUtil.noRetryActorConfig(Constants.SINGLE_PARTITION);
-        actorConfig.setMetricDisabled(true);
-        val actorSystem = TestUtil.actorSystem(tc);
-        val actor = TestUtil.allExceptionActor(counter, sideline, actorConfig, actorSystem);
-        actor.publish(new TestIntMessage(1));
-        Awaitility.await()
-                .timeout(Duration.ofMinutes(1))
-                .catchUncaughtExceptions()
-                .until(() -> counter.get() == 1);
-        val metrics = actorSystem.metricRegistry().getMetrics();
-        assertEquals(0, metrics.size());
+        try {
+            val counter = new AtomicInteger();
+            val sideline = new AtomicBoolean();
+            val actorConfig = TestUtil.noRetryActorConfig(Constants.SINGLE_PARTITION);
+            actorConfig.setMetricDisabled(true);
+            val actorSystem = TestUtil.actorSystem(tc);
+            val actor = TestUtil.allExceptionActor(counter, sideline, actorConfig, actorSystem);
+            actor.publish(new TestIntMessage(1));
+            Awaitility.await()
+                    .timeout(Duration.ofMinutes(1))
+                    .catchUncaughtExceptions()
+                    .until(() -> counter.get() == 1);
+            val metrics = actorSystem.metricRegistry().getMetrics();
+            assertEquals(0, metrics.size());
+        } finally{
+                tc.shutdownNow();
+            }
+        }
     }
-}
