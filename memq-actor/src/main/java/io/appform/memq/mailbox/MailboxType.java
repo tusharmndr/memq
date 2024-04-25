d6be6cab642f81ea02f1ac6a455751792fe4002f
Tushar Mandar <tushar@phonepe.com>
2024-04-25 17:17:47 +0530

    removed unused files

--- memq-actor/src/main/java/io/appform/memq/mailbox/MailboxType.java
+++
@@ -1,7 +0,0 @@
-package io.appform.memq.mailbox;
-
-public enum MailboxType {
-    BOUNDED,
-    UNBOUNDED,
-    ;
-}
--- memq-actor/src/main/java/io/appform/memq/mailbox/config/MailboxConfigVisitor.java
+++
@@ -1,7 +0,0 @@
-package io.appform.memq.mailbox.config;
-
-public interface MailboxConfigVisitor<T> {
-    T visit(BoundedMailboxConfig boundedMailboxConfig);
-
-    T visit(UnBoundedMailboxConfig unBoundedMailboxConfig);
-}
